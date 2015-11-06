840f4ae22b448b9ebc1e1ffb20933d6a8b4c258c
Alexander Hovy <scriptkitti@gmail.com>
2015-11-06 16:23:02 +0200

    Update PHPDownload.php

--- Articles/Files/PHPDownload.php
+++ Articles/Files/PHPDownload.php
@@ -1,9 +1,9 @@
 <?php
-  $Path2File = href;
-  $theFileName = title;
+  $filePath = $_REQUEST['href'];
+  $fileName = $_REQUEST['title'];
   header ('Cache-Control: must-revalidate, post-check=0, pre-check=0');
   header ('Content-Type: application/octet-stream');
-  header ('Content-Length: ' . filesize($Path2File));
-  header ('Content-Disposition: attachment; filename=$theFileName');
+  header ('Content-Length: ' . filesize($filePath));
+  header ('Content-Disposition: attachment; filename=$fileName');
   readfile($Path2File);
 ?>
