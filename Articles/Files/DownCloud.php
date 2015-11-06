d5af12fef2a65562d267a89f096843b59d1c4698
Alexander Hovy <scriptkitti@gmail.com>
2015-11-06 19:07:46 +0200

    Update and rename PHPDownload.php to DownCloud.php

--- Articles/Files/DownCloud.php
+++ Articles/Files/DownCloud.php
@@ -1,7 +1,7 @@
 <?php
-  if (isset($_GET['fileTitle'], $_GET['filePath'])) {
-    $fileTitle = $_GET['fileTitle'];
-    $filePath = $_GET['filePath'];
+  if (isset($_POST['fileTitle'], $_POST['filePath'])) {
+    $fileTitle = $_POST['fileTitle'];
+    $filePath = $_POST['filePath'];
     header('Content-Type: audio/mp3');
     header('Content-Disposition: attachment; filename="' . $fileTitle . '.mp3";');
     header('Pragma: public');
