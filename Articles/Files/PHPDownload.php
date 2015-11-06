fdff7d6e3926bc343b352e41c7a035f2fd7804c9
Alexander Hovy <scriptkitti@gmail.com>
2015-11-06 16:59:33 +0200

    Update PHPDownload.php

--- Articles/Files/PHPDownload.php
+++ Articles/Files/PHPDownload.php
@@ -1,7 +1,11 @@
 <?php
-  $filePath = $_REQUEST['href'];
-  $fileName = $_REQUEST['title'];
-  header ('Content-Type: application/octet-stream');
-  header ('Content-Disposition: attachment; filename=' . $fileName);
+  $stream = $_GET['stream'];
+  $client = $_GET['client'];
+  $fileName = $_GET['title'];
+  $filePath = $stream . '?client_id=' . $client;
+  header('Content-Type: application/octet-stream');
+  header('Content-Disposition: attachment; filename=' . $fileName . ';');
+  header('Content-Length: ' . filesize($filePath));
   readfile($filePath);
+  exit;
 ?>
