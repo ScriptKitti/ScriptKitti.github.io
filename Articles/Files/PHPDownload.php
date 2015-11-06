dfe96e1bed62da22c2ff1c0aa9ed5170fc868887
Alexander Hovy <scriptkitti@gmail.com>
2015-11-06 18:05:50 +0200

    Update PHPDownload.php

--- Articles/Files/PHPDownload.php
+++ Articles/Files/PHPDownload.php
@@ -1,11 +1,11 @@
 <?php
-  $stream = $_GET['stream'];
-  $client = $_GET['client'];
-  $fileName = $_GET['title'];
-  $filePath = $stream . '?client_id=' . $client;
-  header('Content-Type: audio/mp3');
-  header('Content-Disposition: attachment; filename=' . $fileName . '.mp3;');
-  header('Content-Length: ' . filesize($filePath));
-  readfile($filePath);
-  exit;
+   if (isset($_POST['fileTitle'], $_POST['filePath'])) {
+      $fileTitle = $_POST['fileTitle'];
+      $filePath = $_POST['filePath'];
+      header('Content-Type: audio/mp3');
+      header('Content-Disposition: attachment; filename=' . $fileTitle . '.mp3;');
+      header('Content-Length: ' . filesize($filePath));
+      readfile($filePath);
+      exit;
+   }
 ?>
