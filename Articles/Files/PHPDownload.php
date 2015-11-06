67045f49f57365403e54f8aef79799664a7311ea
Alexander Hovy <scriptkitti@gmail.com>
2015-11-06 18:46:35 +0200

    Update PHPDownload.php

--- Articles/Files/PHPDownload.php
+++ Articles/Files/PHPDownload.php
@@ -1,11 +1,13 @@
 <?php
-   if (isset($_POST['fileTitle'], $_POST['filePath'])) {
-      $fileTitle = $_POST['fileTitle'];
-      $filePath = $_POST['filePath'];
-      header('Content-Type: audio/mp3');
-      header('Content-Disposition: attachment; filename=' . $fileTitle . '.mp3;');
-      header('Content-Length: ' . filesize($filePath));
-      readfile($filePath);
-      exit;
-   }
+  if (isset($_POST['fileTitle'], $_POST['filePath'])) {
+    $fileTitle = $_POST['fileTitle'];
+    $filePath = $_POST['filePath'];
+    header('Content-Type: audio/mp3');
+    header('Content-Disposition: attachment; filename="' . $fileTitle . '.mp3";');
+    header('Pragma: public');
+    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+    header('Content-Length: ' . filesize($filePath));
+    readfile($filePath);
+    exit;
+  }
 ?>
