bba7886cc9e3adbb957c962b326f0e1d90e3cad9
Alexander Hovy <scriptkitti@gmail.com>
2015-11-07 14:20:34 +0200

    Update and rename BlobURL.js to BlobDownload.js

---
+++ Articles/Files/BlobDownload.js
@@ -0,0 +1,42 @@
+function download(filePath, fileTitle) {
+  window.URL = window.URL || window.webkitURL;
+  
+  var request = new XMLHttpRequest();
+  request.responseType = 'arraybuffer';
+  request.open('GET', filePath, true);
+  var contentType = response.getResponseHeader('Content-Type') || 'audio/mpeg3';
+  
+  request.onload = function() {
+    var array = new Int8Array(request.response);
+    
+    try {
+      var blob = new Blob([array], {type: contentType});
+      
+      createLink(window.URL.createObjectURL(blob));
+    } catch(error) {
+      window.BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder || window.MSBlobBuilder;
+
+      if (error.name == 'TypeError' && window.BlobBuilder) {
+        var builder = new BlobBuilder();
+        builder.append(array.buffer);
+        var blob = builder.getBlob(contentType);
+        
+        createLink(window.URL.createObjectURL(blob));
+      } else if (error.name == 'InvalidStateError') {
+        var blob = new Blob([array.buffer], {type: contentType});
+        
+        createLink(window.URL.createObjectURL(blob));
+      }
+    }
+  }
+  
+  function createLink(href) {
+    var a = document.createElement('a');
+    a.download = fileTitle;
+    a.href = href;
+    a.target = '_blank';
+    a.click();
+  }
+  
+  request.send();
+}
