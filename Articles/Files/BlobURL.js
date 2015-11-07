0063a6ac646d22218787f73fc721aaad368f2e2b
Alexander Hovy <scriptkitti@gmail.com>
2015-11-07 11:21:28 +0200

    Create BlobURL.js

---
+++ Articles/Files/BlobURL.js
@@ -0,0 +1,24 @@
+function download(data, filename) {
+  if (!window.BlobBuilder && window.WebKitBlobBuilder) {
+    window.BlobBuilder = window.WebKitBlobBuilder;
+  }
+  
+  fs.root.getFile(filename, {
+    create: true
+  }, function(fileEntry) {
+    fileEntry.createWriter(function (fileWriter) {
+      fileWriter.onwriteend = function (e) {
+        console.log('Write completed.');
+      };
+      
+      fileWriter.onerror = function (e) {
+        console.log('Write failed: ' + e.toString());
+      };
+      
+      var builder = new BlobBuilder();
+      builder.append(data);
+      var blob = builder.getBlob();
+      fileWriter.write(blob);
+    }, errorHandler);
+  }, errorHandler);
+}
