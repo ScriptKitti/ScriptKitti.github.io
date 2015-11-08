1e837ad882bb83b772b0ef96b4d9977935cdf8d1
Alexander Hovy <scriptkitti@gmail.com>
2015-11-08 16:39:35 +0200

    Update BlobDownload.js

--- Articles/Files/BlobDownload.js
+++ Articles/Files/BlobDownload.js
@@ -1,43 +1,43 @@
 function download(filePath, fileTitle) {
-  window.URL = window.URL || window.webkitURL;
-  
-  var request = new XMLHttpRequest();
-  request.responseType = 'arraybuffer';
-  request.open('GET', filePath, true);
-  
-  request.onload = function() {
-    var contentType = request.getResponseHeader('Content-Type') || 'audio/mpeg3';
-    var array = new Int8Array(request.response);
+    window.URL = window.URL || window.webkitURL;
     
-    try {
-      var blob = new Blob([array], {type: contentType});
+    var request = new XMLHttpRequest();
+    request.responseType = 'arraybuffer';
+    request.open('GET', filePath, true);
+    
+    request.onload = function() {
+      var contentType = request.getResponseHeader('Content-Type') || 'audio/mpeg3';
+      var array = new Int8Array(request.response);
       
-      createLink(window.URL.createObjectURL(blob));
-    } catch(error) {
-      window.BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder || window.MSBlobBuilder;
-      
-      if (error.name == 'TypeError' && window.BlobBuilder) {
-        var builder = new BlobBuilder();
-        builder.append(array.buffer);
-        var blob = builder.getBlob(contentType);
+      try {
+        var blob = new Blob([array], {type: contentType});
         
         createLink(window.URL.createObjectURL(blob));
-      } else if (error.name == 'InvalidStateError') {
-        var blob = new Blob([array.buffer], {type: contentType});
+      } catch(error) {
+        window.BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder || window.MSBlobBuilder;
         
-        createLink(window.URL.createObjectURL(blob));
+        if (error.name == 'TypeError' && window.BlobBuilder) {
+          var builder = new BlobBuilder();
+          builder.append(array.buffer);
+          var blob = builder.getBlob(contentType);
+          
+          createLink(window.URL.createObjectURL(blob));
+        } else if (error.name == 'InvalidStateError') {
+          var blob = new Blob([array.buffer], {type: contentType});
+          
+          createLink(window.URL.createObjectURL(blob));
+        }
       }
+    };
+    
+    function createLink(href) {
+      var a = document.createElement('a');
+      a.download = fileTitle;
+      a.href = href;
+      a.target = '_blank';
+      a.click();
     }
-  };
-  
-  function createLink(href) {
-    var a = document.createElement('a');
-    a.download = fileTitle;
-    a.href = href;
-    a.target = '_blank';
-    a.click();
+    
+    request.send();
   }
-  
-  request.send();
-}
-alert('Double click on play button to download.');
+  alert('Songs are ready to be downloaded!\n\nDouble click on the play button to download.');
