3c9ded9082ff937d2cb9f4c8bbbba35995007a7c
Alexander Hovy <scriptkitti@gmail.com>
2015-11-10 20:57:24 +0200

    Update FitText.js

--- api/FitText.js
+++ api/FitText.js
@@ -1,2 +1,12 @@
 (function() {
+  $.each($('#fit-text'), function() {
+    var vh = window.screen.height;
+    var vw = window.screen.width;
+    var ratio = vw / vh;
+    var count = $(this).text().length;
+    var size = (vw / count) * ratio;
+    $(this).css({
+      'font-size': size + 'vw'
+    });
+  });
 })();
