c6d8fd1778d55e1cba312f5745598b08e8a820f4
Alexander Hovy <scriptkitti@gmail.com>
2015-11-11 13:53:06 +0200

    Update Parallax.js

--- api/Parallax.js
+++ api/Parallax.js
@@ -1,16 +1,19 @@
 (function() {
-  $(window).scroll(function() {
-    //Parallax
-    if ($(window).scrollTop() >= 0) {
-      imgPos = ($(window).scrollTop() / factor) * -1;
-      $('#background').css({
-        'background-position': 'center ' + imgPos + 'px'
-      });
-    } else {
-      imgSize = imgPerc - ($(window).scrollTop() / factor);
-      $('#background').css({
-        'background-size': imgSize + '%'
-      });
-    }
-  });
+  function(object, factor) {
+    $(window).scroll(function() {
+      if ($(window).scrollTop() >= 0) {
+        var imgPos = ($(window).scrollTop() / factor) * -1;
+        
+        $(object).css({
+          'background-position': 'center ' + imgPos + 'px'
+        });
+      } else {
+        var imgSize = 100 - ($(window).scrollTop() / factor);
+        
+        $(object).css({
+          'background-size': imgSize + '%'
+        });
+      }
+    });
+  }
 })();
