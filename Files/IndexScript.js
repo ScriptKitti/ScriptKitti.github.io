808768b9f5a5221c43422fd0ca2a95d4f524b030
Alexander Hovy <scriptkitti@gmail.com>
2015-11-11 08:52:54 +0200

    Update IndexScript.js

--- Files/IndexScript.js
+++ Files/IndexScript.js
@@ -91,18 +91,3 @@ function getLinkedInCount() {
     $('#linkedin-count').text(linkedin);
   });
 }
-
-(function() {
-  $.each($('#fit-text'), function() {
-    $(this).css({'width': '100%'});
-    var pw = $(this).width();
-    var vh = window.screen.height;
-    var vw = window.screen.width;
-    var ratio = vw / vh;
-    var count = $(this).text().length;
-    var size = (pw / count) * ratio;
-    $(this).css({
-      'font-size': size + 'vw'
-    });
-  });
-})();
