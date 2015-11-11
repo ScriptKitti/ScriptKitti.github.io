f224e8a141ab4fa9ae43f5933cb20bda0fb75642
Alexander Hovy <scriptkitti@gmail.com>
2015-11-11 08:59:37 +0200

    Update IndexScript.js

--- Files/IndexScript.js
+++ Files/IndexScript.js
@@ -95,12 +95,11 @@ function getLinkedInCount() {
 
 function fitText() {
   $.each($('#fit-text'), function() {
-    var pw = $(this).parent().width();
     var vh = window.screen.height;
     var vw = window.screen.width;
     var ratio = vw / vh;
     var count = $(this).text().length;
-    var size = (pw / count) * ratio;
+    var size = (vw / count) * ratio;
     $(this).css({
       'font-size': size + 'vw'
     });
