7e26cbb01384c9dcbde7625388f31271b25a7877
Alexander Hovy <scriptkitti@gmail.com>
2015-11-11 16:42:13 +0200

    Update PostGitHub.js

--- api/PostGitHub.js
+++ api/PostGitHub.js
@@ -1,4 +1,6 @@
-function SKGistAutho(username, password, fileName, _public, content) {
+function SKGistCreate(username, password, fileName, _public, content) {
+  var token, id;
+  
   $.ajax({ 
     url: 'https://api.github.com/authorizations',
     type: 'POST',
@@ -10,11 +12,10 @@ function SKGistAutho(username, password, fileName, _public, content) {
     console.log(error);
     alert('error');
   }).done(function(response) {
-    SKGistCreate(response.id, response.token, fileName, _public, content);
+    token = response.token;
+    id = response.id;
   });
-}
-
-function SKGistCreate(id, token, fileName, _public, content) {
+  
   $.ajax({ 
     url: 'https://api.github.com/gists',
     type: 'POST',
@@ -30,7 +31,24 @@ function SKGistCreate(id, token, fileName, _public, content) {
   });
 }
 
-function SKGistUpdate(id, token, fileName, _public, content) {
+function SKGistUpdate(username, password, fileName, _public, content) {
+  var token, id;
+  
+  $.ajax({ 
+    url: 'https://api.github.com/authorizations',
+    type: 'POST',
+    beforeSend: function(xhr) { 
+      xhr.setRequestHeader('Authorization', 'Basic ' + btoa(username + ':' + password)); 
+    },
+    data: '{"scopes": ["gist"],"note": "Update File: ' + fileName + '"}'
+  }).error(function(error) {
+    console.log(error);
+    alert('error');
+  }).done(function(response) {
+    token = response.token;
+    id = response.id;
+  });
+  
   $.ajax({ 
     url: 'https://api.github.com/gists/' + id,
     type: 'PATCH',
@@ -46,10 +64,11 @@ function SKGistUpdate(id, token, fileName, _public, content) {
     console.log('Update Successful');
   });
 }
+
 //SKGistAutho('scriptkitti@gmail.com', 'Fr0g1-10t', 'happy.json', false, $.parseJSON('{"popularity": {}}'));
 //ID: 0f432777586cecf11b01
 //Token: df9996fa77f2cf2523366a59301fe0272c06e3e0
 //ID: 24375838
 //ID: 15379330
 //Token: e7aa35ecb433c45d7a7bdf64a9753f7ea6b58ed3
-SKGistUpdate('24375838', 'e7aa35ecb433c45d7a7bdf64a9753f7ea6b58ed3', 'happy.json', 'false', 'hi');
+SKGistUpdate('scriptkitti@gmail.com', 'Fr0g1-10t', 'happy.json', false, 'hi');
