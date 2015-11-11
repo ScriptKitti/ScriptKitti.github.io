0f66c12c3e3cdbe11050c0edbb5b71b0da8ee214
Alexander Hovy <scriptkitti@gmail.com>
2015-11-11 12:56:23 +0200

    Update PostGitHub.js

--- api/PostGitHub.js
+++ api/PostGitHub.js
@@ -1,1 +1,11 @@
-
+$.ajax({
+  type: 'POST',
+  url: '',
+  contentType: 'application/json',
+  dataType: 'json',
+  data: JSON.stringify({
+    "count": ""
+  })
+}).done(function() {
+  console.log('Post Complete');
+});
