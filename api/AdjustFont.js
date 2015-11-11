0f01b0e1b43a730a93bb76d62629dbabf772765a
Alexander Hovy <scriptkitti@gmail.com>
2015-11-11 14:23:06 +0200

    Update AdjustFont.js

--- api/AdjustFont.js
+++ api/AdjustFont.js
@@ -1,12 +1,10 @@
-(function() {
-  function SKAdjustFont(object, text, width) {
-    var vh = window.screen.height;
-    var vw = window.screen.width;
-    var ratio = vw / vh;
-    var count = text.length;
-    var size = (width / count) * ratio;
-    $(object).css({
-      'font-size': size + 'vw'
-    });
-  }
-})();
+function SKAdjustFont(object, text, width) {
+  var vh = window.screen.height;
+  var vw = window.screen.width;
+  var ratio = vw / vh;
+  var count = text.length;
+  var size = (width / count) * ratio;
+  $(object).css({
+    'font-size': size + 'vw'
+  });
+}
