c9166def6a824f963739076233f6c29bef9eaada
Alexander Hovy <scriptkitti@gmail.com>
2015-11-11 08:59:17 +0200

    Update FitText.js

--- api/FitText.js
+++ api/FitText.js
@@ -1,12 +1,11 @@
 (function() {
   function fitText() {
     $.each($('#fit-text'), function() {
-      var pw = $(this).parent().width();
       var vh = window.screen.height;
       var vw = window.screen.width;
       var ratio = vw / vh;
       var count = $(this).text().length;
-      var size = (pw / count) * ratio;
+      var size = (vw / count) * ratio;
       $(this).css({
         'font-size': size + 'vw'
       });
