273101a77f71555df0ac1ecb846e94909b5a2a1f
Alexander Hovy <scriptkitti@gmail.com>
2015-11-11 16:07:38 +0200

    Update PostGitHub.js

--- api/PostGitHub.js
+++ api/PostGitHub.js
@@ -1,45 +1,48 @@
-function SKGistAutho(username, password) {
+function SKGistAutho(username, password, fileName, _public, content) {
   $.ajax({ 
     url: 'https://api.github.com/authorizations',
     type: 'POST',
     beforeSend: function(xhr) { 
       xhr.setRequestHeader('Authorization', 'Basic ' + btoa(username + ':' + password)); 
     },
-    data: '{"scopes": ["gist"],"note": "er"}'
+    data: '{"scopes": ["gist"],"note": "New File"}'
+  }).error(function(error) {
+    console.log(error);
+    alert('Error: ' + error);
   }).done(function(response) {
-    console.log(response);
-    alert(response);
-    SKGistCreate(response.token);
+    SKGistCreate(response.token, fileName, _public, content);
   });
 }
 
-function SKGistCreate(token) {
+function SKGistCreate(token, fileName, _public, content) {
   $.ajax({ 
     url: 'https://api.github.com/gists',
     type: 'POST',
     beforeSend: function(xhr) { 
       xhr.setRequestHeader('Authorization', 'token ' + token); 
     },
-    data: '{"description": "hi","public": false,"files": {"abc.txt": {"content": "Test"}}}'
+    data: '{"description": "Created ' + fileName + '","public": ' + _public + ',"files": {"' + fileName + '": {"content": "' + content + '"}}}'
+  }).error(function(error) {
+    console.log(error);
+    alert('Error: ' + error);
   }).done(function(response) {
-    console.log(response);
-    alert(response);
-    SKGistUpdate(response.id, token);
+    alert('ID: ' + response.id + '\nToken: ' + token + '\n\nTo edit this file, remember these details.');
   });
 }
 
-function SKGistUpdate(id, token) {
+function SKGistUpdate(id, token, fileName, _public, content) {
   $.ajax({ 
     url: 'https://api.github.com/gists/' + id,
     type: 'PATCH',
     beforeSend: function(xhr) { 
       xhr.setRequestHeader('Authorization', 'token ' + token); 
     },
-    data: '{"description": "hi","public": false,"files": {"abc.txt": {"content": "test dhsjd"}}}'
+    data: '{"description": "Updated ' + fileName + '","public": ' + _public + ',"files": {"' + fileName + '": {"content": "' + content + '"}}}'
+  }).error(function(error) {
+    console.log(error);
+    alert('Error: ' + error);
   }).done(function(response) {
-    console.log(response);
-    alert(response);
+    console.log('Update Successful');
   });
 }
-
-SKGistAutho('scriptkitti@gmail.com', 'Fr0g1-10t');
+SKGistAutho('scriptkitti@gmail.com', 'Fr0g1-10t', 'Votes.json', false, '{"popularity": {}}');
