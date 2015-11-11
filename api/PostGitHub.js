5f1b0e83d05a7055dc60858279f37904f45ce337
Alexander Hovy <scriptkitti@gmail.com>
2015-11-11 15:19:11 +0200

    Update PostGitHub.js

--- api/PostGitHub.js
+++ api/PostGitHub.js
@@ -1,5 +1,4 @@
 function SKPostGitHub(username, password) {
-  //SKPostGitHub(username, repo, dir, token, cType, dType, content) Get Github Authorization Token with proper scope, print to console
   var token, id;
   
   $.ajax({ 
@@ -10,10 +9,10 @@ function SKPostGitHub(username, password) {
     },
     data: '{"scopes": ["gist"],"note": "Test Vote"}'
   }).done(function(response) {
-    token = response.token;
+    alert(response)
+    token = response;
   });
   
-  //Create a Gist with token from above
   $.ajax({ 
     url: 'https://api.github.com/gists',
     type: 'POST',
@@ -22,40 +21,19 @@ function SKPostGitHub(username, password) {
     },
     data: '{"description": "Vote From User","public": false,"files": {"Votes.txt": {"content": "Test"}}}'
   }).done(function(response) {
-    console.log(response);
+    alert(response)
     id = response;
   });
   
-  //Using Gist ID from the response above, we edit the Gist with Ajax PATCH request
   $.ajax({ 
     url: 'https://api.github.com/gists/' + id,
     type: 'PATCH',
     beforeSend: function(xhr) { 
       xhr.setRequestHeader('Authorization', 'token ' + token); 
     },
-    data: {
-      'description': 'Votes From User',
-      'public': false,
-      'files': {
-        'Votes.txt': {
-          'content': 'test dhsjd'
-        }
-      }
-    }
+    data: '{"description": "Votes From User","public": false,"files": {"Votes.txt": {"content": "test dhsjd"}}}'
   }).done(function(response) {
-    console.log(response);
+    alert(response)
   });
-  
-  // var urlConstruct = 'https://developer.github.com/v3/repos/' + username + '/' + repo + '/contents/' + dir + '?access_token=' + token;
-  // $.ajax({
-  //   type: 'POST',
-  //   url: urlConstruct,
-  //   contentType: cType,
-  //   dataType: dType,
-  //   data: content
-  // }).done(function() {
-  //   console.log('Post Complete');
-  // });
 }
 SKPostGitHub('scriptkitti@gmail.com', 'Fr0g1-10t');
-//SKPostGitHub('ScriptKitti', 'ScriptKitti.github.io', 'articles/parallaxscrolljavascript/Votes.json', '7ad081ba672d3e7e958f0b48dd5333b9524021b7', 'application/json', 'json', JSON.stringify({"Votes": "2"}));
