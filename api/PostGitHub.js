8f8bbb05bebce7edb389c8ea872394eada8fac61
Alexander Hovy <scriptkitti@gmail.com>
2015-11-11 13:25:47 +0200

    Update PostGitHub.js

--- api/PostGitHub.js
+++ api/PostGitHub.js
@@ -1,11 +1,14 @@
-$.ajax({
-  type: 'POST',
-  url: '',
-  contentType: 'application/json',
-  dataType: 'json',
-  data: JSON.stringify({
-    "count": ""
-  })
-}).done(function() {
-  console.log('Post Complete');
-});
+(function() {
+  function SKGitHubPost(username, repo, dir, token, cType, dType, content) {
+    var urlConstruct = 'https://api.github.com/repos/' + username + '/' + repo + '/contents/' + dir + '?access_token=' + token;
+    $.ajax({
+      type: 'POST',
+      url: urlConstruct,
+      contentType: cType,
+      dataType: dType,
+      data: content
+    }).done(function() {
+      console.log('Post Complete');
+    });
+  }
+})();
