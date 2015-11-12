6c96f10a7b440b0efc8b00059499efa6d339d551
Alexander Hovy <scriptkitti@gmail.com>
2015-11-12 19:35:32 +0200

    Update InternetSpeed.js

--- api/InternetSpeed.js
+++ api/InternetSpeed.js
@@ -1,48 +1,76 @@
-function SKPingTest() {
-  return ;
+function SKPngTest(object) {
+  var start, end, ping;
+  
+  start = (new Date()).getTime();
+  
+  $.ajax({
+    url: '',
+    success: function(result) {
+      end = (new Date()).getTime();
+      ping = end - start;
+      $(object).text(ping + ' ms');
+    }
+  });
 }
 
 function SKDownloadTest(object, size) {
-  var imageURL = 'http://scriptkitti.github.io/Images/ScriptKitti.png';
-  var start, end, antiCache;
-  var imageSize = 174057;
+  var imageURL = 'http://scriptkitti.github.io/Images/ScriptKittiTitle.jpg';
+  var start, end, ping, newSize, oldSize, timer, antiCache;
   
   antiCache = '?antiCache=' + (new Date()).getTime();
   imageURL = imageURL + antiCache;
+  
+  start = (new Date()).getTime();
+  
+  $.ajax({
+    url: '',
+    success: function(result) {
+      end = (new Date()).getTime();
+      ping = (end - start) / 1000;
+    }
+  });
   
   if (XMLHttpRequest) {
     var request = new XMLHttpRequest();
   } else if (ActiveXObject) {
     var request = new ActiveXObject('Microsoft.XMLHTTP');
   }
-
+  
+  request.onprogress = function(event) {    
+    newSize = event.loaded;
+    
+    timer = function() {
+      oldSize = event.loaded;
+    };
+    window.setTimeout(timer, 1000);
+    
+    var length = (newSize - oldSize) * 8;
+    var bps = (length / ping).toFixed(2);
+    var kbps = (bps / 1024).toFixed(2);
+    var mbps = (kbps / 1024).toFixed(2);
+    
+    size = size.toLowerCase();
+    
+    if (size == 'bps') {
+      $(object).text(bps + ' bps');
+    } else if (size == 'kbps') {
+      $(object).text(kbps + ' kbps');
+    } else {
+      $(object).text(mbps + ' Mbps');
+    }
+  }
+  
   request.responseType = 'arraybuffer';
   request.open('GET', imageURL, true);
-  var contentType = request.getResponseHeader('Content-Type') || 'image/png';
+  var contentType = request.getResponseHeader('Content-Type') || 'image/jpeg';
   request.send();
-
+  
   request.onreadystatechange = function() {
     if (request.status == 200) {
       if (request.readyState == 2) {
-        start = (new Date()).getTime();
-      }
-      
-      end = (new Date()).getTime();
-      
-      var duration = (end - start) / 1000;
-      var length = imageSize * 8;
-      var bps = (length / duration).toFixed(2);
-      var kbps = (bps / 1024).toFixed(2);
-      var mbps = (kbps / 1024).toFixed(2);
-      
-      size = size.toLowerCase();
-      
-      if (size == 'bps') {
-        $(object).text(bps + 'bps');
-      } else if (size == 'kbps') {
-        $(object).text(kbps + 'kbps');
-      } else {
-        $(object).text(mbps + 'Mbps');
+        console.log('Ready');
+      } else if (request.readyState == 4) {
+        console.log('Complete');
       }
     } else {
       $(object).text('Page Not Found');
